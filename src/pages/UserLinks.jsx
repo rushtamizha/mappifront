d34728317a86075cd113bb5431a877ce37b10651
rushtamizha <159513587+rushtamizha@users.noreply.github.com>
2025-08-14 13:22:26 +0530

    Update UserLinks.jsx

--- src/pages/UserLinks.jsx
+++ src/pages/UserLinks.jsx
@@ -25,7 +25,7 @@ import toast from 'react-hot-toast';
 const cloudName = import.meta.env.VITE_CLOUD_NAME;
 const uploadPreset = import.meta.env.VITE_UPLOAD_PRESET;
 import avatar from '../assets/icons/avatar.avif'
-import verified from "../assets/icons/verify.png"
+
 // ✅ Extract YouTube video ID
 const getYouTubeVideoId = (url) => {
   try {
@@ -131,6 +131,7 @@ const UserLinks = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [username, setUsername] = useState('');
   const [addloading,setAddloading] = useState(false)
+    const [loader,setLoader] = useState(false)
   const sensors = useSensors(
     useSensor(TouchSensor),
     useSensor(PointerSensor),
@@ -138,6 +139,7 @@ const UserLinks = () => {
   );
 
   const fetchLinks = async () => {
+    setLoader(true)
     const token = localStorage.getItem('token');
     if (!token) return navigate('/login');
     const decoded = jwtDecode(token);
@@ -148,6 +150,7 @@ const UserLinks = () => {
     console.log(profile)
     setLinks(data.data.links);
     setFilteredLinks(data.data.links);
+    setLoader(false)
   };
 
   useEffect(() => {
@@ -188,8 +191,8 @@ const UserLinks = () => {
     setNewLink({ title: '', url: '', description: '', logo: '' });
     toast.success('Link added successfully')
     } catch (error) {
-      console.log(error)
-      toast.success('Link  successfully')
+      console.log(error.response.data.error)
+     toast.error(error.response.data.error)
     }finally{
       setShowForm(false);
       fetchLinks();
@@ -272,6 +275,17 @@ const handleLogoUpload = async (e) => {
   const videoId = getYouTubeVideoId(newLink.url);
 
   return (
+    loader ? (
+    <div className="loader h-full w-full"> 
+      <div className='flex justify-center items-center h-full fixed w-full '>
+        <div role="status">
+    <svg aria-hidden="true" className="inline w-8 h-8 text-gray-100 animate-spin  fill-emerald-700" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+    </div>
+      </div>
+    </div> ) : (
     <>
     <div className='p-2 flex gap-2 max-w-2xl mx-auto  '>
       <div class="relative w-full">
@@ -312,7 +326,7 @@ const handleLogoUpload = async (e) => {
 </div>
 
       <div>
-        <div className='big-text font-semibold text-gray-700 font-display capitalize font-stretch-105%'>{profile.username} <img className='h-3 inline' src={profile.plan=='pro' || profile.plan=='premium'?verified:''} alt="" /></div>
+        <div className='big-text font-semibold text-gray-700 font-display capitalize font-stretch-105%'>{profile.username} <img className='h-3 inline' src={profile.plan=='pro' || profile.plan=='premium'?'/src/assets/verify.png':''} alt="" /></div>
         <div className=' text-gray-500 capitalize font-stretch-105%'>{profile.bio}</div>
       </div>
     </div>
@@ -455,9 +469,10 @@ const handleLogoUpload = async (e) => {
         </SortableContext>
       </DndContext>
     </div>
-    </>
+    </> )
   );
 };
 
 export default UserLinks;
+
 
